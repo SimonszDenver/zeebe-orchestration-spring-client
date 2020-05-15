b804b2eef7567d1a852204c6c7f825b9d1c4c977
stelan.slt@gmail.com <stelan@578951>
2020-05-15 21:35:10 +0530

    first commit

---
+++ src/main/java/com/objectone/orchestratorclient/OrchestratorClientApplication.java
@@ -0,0 +1,13 @@
+package com.objectone.orchestratorclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestratorClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/objectone/orchestratorclient/OrchestratorClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.objectone.orchestratorclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
