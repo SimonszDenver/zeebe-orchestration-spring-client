f962344d7232e4308a3b1beec144d3e395251f91
stelan.slt@gmail.com <stelan@578951>
2020-05-16 23:49:56 +0530

    zeebe error fixed and connected to ec2

--- src/main/java/com/objectone/orchestratorclient/api/EngineResource.java
+++ src/main/java/com/objectone/orchestratorclient/api/EngineResource.java
@@ -1,17 +1,39 @@
 package com.objectone.orchestratorclient.api;
 
+import io.zeebe.client.api.response.WorkflowInstanceEvent;
 import io.zeebe.spring.client.ZeebeClientLifecycle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
 public class EngineResource {
 
+    private final Logger log = LoggerFactory.getLogger(EngineResource.class);
+
     @Autowired
     private ZeebeClientLifecycle client;
 
+    @GetMapping("deploy")
+    public void deploy() {
+        final WorkflowInstanceEvent event =
+                client
+                        .newCreateInstanceCommand()
+                        .bpmnProcessId("order-process")
+                        .latestVersion()
+                        .variables("{\"a\": \"" + UUID.randomUUID().toString() + "\"}")
+                        .send()
+                        .join();
+
+        log.info("started instance for workflowKey='{}', bpmnProcessId='{}', version='{}' with workflowInstanceKey='{}'",
+                event.getWorkflowKey(), event.getBpmnProcessId(), event.getVersion(), event.getWorkflowInstanceKey());
+    }
 
 
 }
